26ff0bbeb02b24fbe361311abc007cdfa94fdb59
Harsha Vardhan <x.projekt@outlook.com>
2025-01-13 16:51:22 +0530

    feat: add Header & Footer source-files

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import '/src/css/footer.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function Footer() {
+    return (
+        <footer>
+            <small>Copyright Slush Works 2024</small>
+        </footer>
+    );
+}
+
+export default Footer;
