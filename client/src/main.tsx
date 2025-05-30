77b6cd96316fc3b0977bb569be329e0ba35d0afb
Harsha Vardhan <x.projekt@outlook.com>
2025-05-30 15:01:18 +0530

    feat: remove old source-files

--- client/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import "./styles/text-color.css"; // Import additional styles for text color consistency
-
-createRoot(document.getElementById("root")!).render(<App />);
